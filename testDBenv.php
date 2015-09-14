b7d8ae30e4c4b7d9ad841faef6a3c6d363b2e535
Ian_M_Porter <Ian_M_Porter@dell.com>
2015-09-14 14:47:02 +0100

    message

---
+++ testDBenv.php
@@ -0,0 +1,32 @@
+<?php
+
+    require_once("./src/config.php");
+
+    //comment
+    class DBEnvTest {
+
+        private $db = NULL;
+        private $mysqli = NULL;
+        public function __construct(){
+            $this->test();					// Initiate Database connection
+        }
+
+
+        private function test(){
+
+                
+            echo 'host: ' . DB_HOST . '<br>';
+            ehco 'port: ' .DB_PORT . '<br>';
+            echo 'user: ' . DB_USER . '<br>';
+            echo 'password: ' . DB_PASS . '<br>';
+            echo 'db: ' . DB_BASE . '<br>';
+
+            
+        }   
+
+    } 
+    
+    $test = new DBEnvTest();
+    $test->test();
+
+?>
