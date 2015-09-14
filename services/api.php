b72921370b6a8139bd94664f3f8ebf03bebe5abe
Ian_M_Porter <Ian_M_Porter@dell.com>
2015-09-14 15:58:20 +0100

    message

--- services/api.php
+++ services/api.php
@@ -49,14 +49,16 @@
             $password = "password123"; #$headers['Auth-Password'];
             $query="SELECT uid, name, email FROM users WHERE email = '$email' AND password = '".md5($password)."' LIMIT 1";    
             $r = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
-            if($r->num_rows > 0) {
+       /*     if($r->num_rows > 0) {
 						$result = $r->fetch_assoc();	
                         #header("Auth-token : token1234");
 						$this->response($this->json($result), 200);
 					} 
                     else { 
 					   $this->response($this->json($error), 401);
-                    }
+                    }  */
+            
+            $this->response($this->"OK", 200)
             
             /*
 			if(!empty($email) and !empty($password)){
