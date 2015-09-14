75b637b2d4848b7ed3d1e5638f0efb1397989b38
Ian_M_Porter <Ian_M_Porter@dell.com>
2015-09-14 14:52:52 +0100

    message

--- src/config.php
+++ src/config.php
@@ -1,7 +1,7 @@
 <?php
-	define('DB_HOST', getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_HOST"););
+	define('DB_HOST', getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_HOST"));
 	define('DB_USER',getenv('DATABASE_USER'));
 	define('DB_PASS',getenv('DATABASE_PASSWORD'));
 	define('DB_BASE', getenv("DATABASE_NAME"));
-	define('DB_PORT',getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_PORT");); 
+	define('DB_PORT',getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_PORT")); 
 ?>
