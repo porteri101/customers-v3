ca06c7c3494f71ed1b05ac9fe32e5438d25570ba
Ian_M_Porter <Ian_M_Porter@dell.com>
2015-09-14 14:56:20 +0100

    message

--- testDBenv.php
+++ testDBenv.php
@@ -13,20 +13,17 @@
 
 
         private function test(){
-
-                
+      
             echo 'host: ' . DB_HOST . '<br>';
             echo 'port: ' . DB_PORT . '<br>';
             echo 'user: ' . DB_USER . '<br>';
             echo 'password: ' . DB_PASS . '<br>';
             echo 'db: ' . DB_BASE . '<br>';
-
             
         }   
 
     } 
     
     $test = new DBEnvTest();
-    $test->test();
 
 ?>
